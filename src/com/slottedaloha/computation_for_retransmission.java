7a03009bede3d263243260a2e06f96767b79e750
Amit Samanta <amit.samanta049@gmail.com>
2018-03-16 12:08:13 +0530

    Update copyright information

--- src/com/slottedaloha/computation_for_retransmission.java
+++ src/com/slottedaloha/computation_for_retransmission.java
@@ -1,3 +1,19 @@
+/* Computation of retransmission
+ *
+ *
+ * Initial simulation code from Amit Samanta (IIT Kharagpur).
+ *
+ * Author:
+ *
+ *	Amit Samanta <amit.samanta049@gmal.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ */
+
+
 package com.slottedaloha;
 
 public class computation_for_retransmission {
