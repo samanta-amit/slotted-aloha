5c14aad24e3cf6ce6a7ce4977a3aa49faab5acb7
Amit Samanta <amit.samanta049@gmail.com>
2018-03-16 12:03:55 +0530

    Update copyright information

--- src/com/slottedaloha/computation_of_b000.java
+++ src/com/slottedaloha/computation_of_b000.java
@@ -1,3 +1,19 @@
+/* Computation of b000 using Markov model
+ *
+ *
+ * Initial simulation code from Amit Samanta (IIT Kharagpur).
+ *
+ * Authors:
+ *
+ *	Amit Samanta <amit.samanta049@gmal.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ */
+
+
 package com.slottedaloha;
 
 public class computation_of_b000 {
