159293114f1afa77bab6edd354c3aefe5eebfe67
Amit Samanta <amit.samanta049@gmail.com>
2018-03-16 12:05:09 +0530

    Update copyright information

--- src/com/slottedaloha/computation_of_average_delay.java
+++ src/com/slottedaloha/computation_of_average_delay.java
@@ -1,3 +1,20 @@
+/* Estimation of avergae node under non-idel channel conditions using Markov model
+ *
+ *
+ * Initial simulation code from Amit Samanta (IIT Kharagpur).
+ *
+ * Authors:
+ *
+ *	Amit Samanta <amit.samanta049@gmal.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ */
+
+
+
 package com.slottedaloha;
 
 public class computation_of_average_delay {
