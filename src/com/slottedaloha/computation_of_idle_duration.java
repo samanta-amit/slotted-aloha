5c76026d582e1c49a57e0106d3b2a00e13bcb85f
Amit Samanta <amit.samanta049@gmail.com>
2018-06-13 16:33:36 +0530

    Minor change

--- src/com/slottedaloha/computation_of_idle_duration.java
+++ src/com/slottedaloha/computation_of_idle_duration.java
@@ -1,3 +1,18 @@
+/* Computation of idle duration
+ *
+ *
+ * Initial simulation code from Amit Samanta (IIT Kharagpur).
+ *
+ * Author:
+ *
+ *	Amit Samanta <amit.samanta049@gmal.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ */
+
 package com.slottedaloha;
 
 public class computation_of_idle_duration {
